f8bd91c0d0b422e72a46d96bcf1b42840fa84c7a
Nazar Matviichuk <matvichuk231488@gmail.com>
2026-08-21 17:24:06 +0300

    end homework

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const formData = {
+  email: '',
+  message: '',
+};
+const LS_Key = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = form.elements.email;
+const textarea = form.elements.message;
+
+const saved = JSON.parse(localStorage.getItem(LS_Key) || '{}');
+input.value = saved.email || '';
+textarea.value = saved.message || '';
+
+form.addEventListener('input', event => {
+  if (event.target.name === 'email') {
+    formData.email = event.target.value;
+  } else {
+    formData.message = event.target.value;
+  }
+  localStorage.setItem(LS_Key, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (
+    event.target.elements.message.value === '' ||
+    event.target.elements.email.value === ''
+  ) {
+    return alert('Fill please all fields');
+  }
+  localStorage.removeItem(LS_Key);
+  form.reset();
+});
